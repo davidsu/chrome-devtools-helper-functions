c707505fc55afb92019a17890e408e80e21a1dbd
David <davidsu@wix.com>
2017-05-14 15:06:01 +0300

    inject in previewframe (well, try to... if it's domain differs from main iframe it throws.... need to fix it)

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-function printJsonCircular(o) {
-    var cache = [];
-    var a = JSON.stringify(o, function(key, value) {
-        if (typeof value === 'object' && value !== null) {
-            if (cache.indexOf(value) !== -1) {
-                // Circular reference found, discard key
-                return;
+function utilsInject(){
+    function printJsonCircular(o) {
+        var cache = [];
+        var a = JSON.stringify(o, function(key, value) {
+            if (typeof value === 'object' && value !== null) {
+                if (cache.indexOf(value) !== -1) {
+                    // Circular reference found, discard key
+                    return;
+                }
+                // Store value in our collection
+                cache.push(value);
+            } else {
+                var functionObj = {}
+                functionObj['function' + key] = key
+                cache.push(functionObj)
             }
-            // Store value in our collection
-            cache.push(value);
-        } else {
-            var functionObj = {}
-            functionObj['function' + key] = key
-            cache.push(functionObj)
-        }
-        return value;
-    }, 4);
-    return a;
-}
-
-function findPath(_obj, prop, exactOnly = false, limit = 10){
-    var foundCount = 0
-    var seen = new Set()
-    function _findPath(obj, currpath){
-        if(foundCount > limit){
-            return
-        }
-        if(!obj || seen.has(obj) || /requirejs\.s/.test(currpath) || currpath.length > 130){
-            return
-        }
-        seen.add(obj)
-        for (var k in obj){
-            var nextPath = currpath + '.' + k
-            if(k === prop || (!exactOnly && k.toLowerCase().indexOf(prop.toLowerCase())!=-1)){
-                console.log(nextPath)
-                foundCount++
-            }
-            try{
-            _findPath(obj[k], nextPath)
-            }catch(e){}
-        }    
+            return value;
+        }, 4);
+        return a;
     }
-    _findPath(_obj, '')
-}
 
-window.printJsonCircular = printJsonCircular
-window.findPath = findPath
-console.log('loading extension')
+    function findPath(_obj, prop, exactOnly = false, limit = 10){
+        var foundCount = 0
+        var seen = new Set()
+        function _findPath(obj, currpath){
+            if(foundCount > limit){
+                return
+            }
+            if(!obj || seen.has(obj) || /requirejs\.s/.test(currpath) || currpath.length > 130){
+                return
+            }
+            seen.add(obj)
+            for (var k in obj){
+                var nextPath = currpath + '.' + k
+                if(k === prop || (!exactOnly && k.toLowerCase().indexOf(prop.toLowerCase())!=-1)){
+                    console.log(nextPath)
+                    foundCount++
+                }
+                try{
+                _findPath(obj[k], nextPath)
+                }catch(e){}
+            }    
+        }
+        _findPath(_obj, '')
+    }
+
+    window.printJsonCircular = printJsonCircular
+    window.findPath = findPath
+
+    function injectInPreviewFrame(count){
+        if(count>30) return;
+
+        const preview = document.querySelector('#preview')
+        if(preview && preview.contentWindow){
+            preview.contentWindow.findPath = findPath
+        } else {
+            setTimeout(() => injectInPreviewFrame(count+1), 1000)
+        }
+    }
+    injectInPreviewFrame(0)
+    console.log('loading extension')
+}
+window.utilsInject = utilsInject;
+utilsInject();
